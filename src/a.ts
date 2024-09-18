784f46bff437d2a49843436a378f641b4b1b1f44
Pushkar Gupta <pushkardps8@gmail.com>
2024-09-18 16:31:09 +0530

    Implemented generics

--- src/a.ts
+++ src/a.ts
@@ -162,3 +162,28 @@ enum ResponseStatus {
        // and so on...
 // 		res.status(ResponseStatus.Success).json({});
 // })
+
+
+// GENERICS
+
+function getFirstElement(arr: (string | number)[]) {
+    return arr[0];
+}
+
+const el = getFirstElement([1, 2, 3]);
+
+
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+let output1 = identity<string>("myString");
+let output2 = identity<number>(100);
+
+
+function getFirstElement1<T>(arr: T[]) {
+    return arr[0];
+}
+
+const el1 = getFirstElement1<String>(["harkiratSingh", "ramanSingh"]);
+console.log(el1.toLowerCase())
